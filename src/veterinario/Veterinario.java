59a3f510b3b7fdd4b73455c23003a135f920f057
Javi Palacios <javi@fjp.es>
2018-05-24 16:06:56 +0200

    Estructura del proyecto Java con NetBeans

---
+++ src/veterinario/Veterinario.java
@@ -0,0 +1,16 @@
+package veterinario;
+
+/**
+ *
+ * @author Javi Palacios <javi@fjp.es>
+ */
+public class Veterinario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
