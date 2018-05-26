31e296d8c052c1178cc7c33fc3fa4616f1b3ee05
Javi Palacios <javi@fjp.es>
2018-05-27 01:02:33 +0200

    Creando la clase Cliente

--- src/oovv/Cliente.java
+++ src/oovv/Cliente.java
@@ -10,5 +10,48 @@ package oovv;
  * @author alumno1718
  */
 public class Cliente {
-    
+
+    private String DNI;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String telefono;
+    private String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(String DNI, String nombre, String apellidos, String direccion, String telefono, String correo) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
 }
