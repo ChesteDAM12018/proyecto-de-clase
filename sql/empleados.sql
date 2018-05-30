78e012978ce2e4dd4c00f5a895e80532f6286d24
Nico Tena <nicotena@gmail.com>
2018-05-30 09:40:56 +0200

    Create empleados.sql

---
+++ sql/empleados.sql
@@ -0,0 +1,5 @@
+INSERT INTO empleados(dni, nombre, apellidos, telefono, sueldo, tipo) VALUES ('01478966Z', 'Paquita', 'Salas Mas', '692177459', '5000', 'Veterinario');
+INSERT INTO empleados(dni, nombre, apellidos, telefono, sueldo, tipo) VALUES ('54302544P', 'David', 'Simón Hernández', '714533698', '1000', 'Administrativo');
+INSERT INTO empleados(dni, nombre, apellidos, telefono, sueldo, tipo) VALUES ('36478211T', 'Ana', 'Martinez Gómez', '623144782', '2000', 'Auxiliar');
+INSERT INTO empleados(dni, nombre, apellidos, telefono, sueldo, tipo) VALUES ('96321748B', 'Luisa', 'Herraez Andrés', '621477932', '1700', 'Recepcionista');
+INSERT INTO empleados(dni, nombre, apellidos, telefono, sueldo, tipo) VALUES ('23685477C', 'Sara', 'Berlanga Planells', '632144786', '4500', 'Veterinario');
