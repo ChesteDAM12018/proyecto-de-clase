624a320f3e48ca1c2bfed60aeac5d83edb0fa813
Rubén Golfe <rubengolfe@gmail.com>
2018-05-30 10:12:11 +0200

    Ventana principal y su lógica

---
+++ src/logica/VentanaLOG.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.VentanaP;
+
+/**
+ *
+ * @author ruben
+ */
+public class VentanaLOG implements ActionListener {
+
+    private VentanaP ventana;
+    private VentanaLOGctr ctl;
+    private String dni;
+
+    public VentanaLOG() {
+        ventana = new VentanaP();
+        ventana.setOyente(this);
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Validar":
+                    dni = ventana.getDni();
+                    ctl = new VentanaLOGctr(ventana);
+                    System.out.println(dni);
+                break;
+            case "Cancelar":
+                    System.exit(0);
+                break;
+        }
+    }
+
+}
