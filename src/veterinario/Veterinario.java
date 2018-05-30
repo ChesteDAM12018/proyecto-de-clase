73a4b5367c8c8770a859623e0643fbd5ec53585f
Javi Palacios <javi@fjp.es>
2018-05-30 18:16:19 +0200

    Añadiendo la llamada a la ventana principal desde el main

--- src/veterinario/Veterinario.java
+++ src/veterinario/Veterinario.java
@@ -1,12 +1,14 @@
 package veterinario;
 
+import logica.VentanaLOG;
+
 public class Veterinario {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VentanaLOG();
     }
 
 }
